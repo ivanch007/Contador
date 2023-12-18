40459d836fe94fae0eca35851da740b843164ed6
Ivan Ocampo <ivanocampo07@gmail.com>
2023-12-18 17:37:32 -0500

    Primer programa creado con React.js

---
+++ src/HookApp.jsx
@@ -0,0 +1,11 @@
+import { ContadorCounter } from "./components/ContadorCounter"
+
+export const HookApp = () => {
+  return (
+    <>
+      <h1>Aplicación para practicar Hooks</h1>
+      <hr />
+      <ContadorCounter></ContadorCounter>
+    </>
+  )
+}
